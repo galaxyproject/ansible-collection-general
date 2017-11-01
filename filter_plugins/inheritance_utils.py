4d79d30e5f9b9880958f365a85f122d07999d8c2
Nate Coraor <nate@bx.psu.edu>
2017-11-01 11:38:57 -0400

    Reworked inheritance var merging once more, because vars were not templated in the previous incarnation. Implementing chaining as a lookup plugin seems a little cleaner and has automatic access to vars. Subkey merging remains a filter plugin.

--- filter_plugins/inheritance_utils.py
+++ filter_plugins/inheritance_utils.py
@@ -4,6 +4,9 @@ from collections import OrderedDict
 def subelement_union(chain, key, subkey):
     # this will not merge any keys other than subkey, and subkey must be a list
     r = OrderedDict()
+    # if a lookup plugin is called in the variable context and returns a
+    # one-element list, the return value is replaced with the element
+    chain = [chain] if not isinstance(chain, list) else chain
     for d in chain:
         if d[key] in r:
             r[d[key]][subkey].extend(filter(lambda i: i not in r[d[key]][subkey], d[subkey]))
@@ -12,17 +15,8 @@ def subelement_union(chain, key, subkey):
     return list(r.values())
 
 
-def chain_vars(base, _vars):
-    r = []
-    gprefixes = [n + '_group' for n in _vars['group_names']]
-    for prefix in ['all', 'group'] + gprefixes + ['host']:
-        r.extend(_vars.get(prefix + '_' + base, []))
-    return r
-
-
 class FilterModule(object):
     def filters(self):
         return {
             'subelement_union': subelement_union,
-            'chain_vars': chain_vars,
         }
---
+++ lookup_plugins/inheritance_chain.py
@@ -0,0 +1,51 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+
+DOCUMENTATION = """
+"""
+
+EXAMPLES = """
+"""
+
+RETURN = """
+"""
+
+
+from ansible.errors import AnsibleError
+from ansible.plugins.lookup import LookupBase
+
+try:
+    from __main__ import display
+except ImportError:
+    from ansible.utils.display import Display
+    display = Display()
+
+
+class LookupModule(LookupBase):
+    def run(self, terms, variables=None, **kwargs):
+        try:
+            assert 'group_names' in variables, "Missing 'group_names' in variables"
+            assert len(terms) == 1, "Inheritance chain lookup plugin expects 1 term, got %s" % len(terms)
+        except AssertionError as exc:
+            raise AnsibleError(str(exc))
+        _vars = variables or {}
+        base = terms[0]
+        r = []
+        for prefix in ['all', 'group'] + [n + '_group' for n in _vars['group_names']] + ['host']:
+            _var = prefix + '_' + base
+            _val = _vars.get(_var, [])
+            if _val:
+                display.vvvv('%s = %s' % (_var, _val))
+            try:
+                t = self._templar.template(
+                    _vars.get(_var, []), preserve_trailing_newlines=True,
+                    convert_data=True, escape_backslashes=False)
+            except Exception as exc:
+                raise AnsibleError("Templating '%s' failed: %s" % (_var, str(exc)))
+            if t != _val:
+                display.vvvv('%s -> %s' % (_var, t))
+            r.extend(t)
+        if not r:
+            raise AnsibleError("No inheritance chain variables exist for base '%s'" % base)
+        return r
