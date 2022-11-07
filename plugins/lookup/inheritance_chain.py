5f2b78fc6d18281e9029eee1ac9272d7bf7bfceb
Nate Coraor <nate@bx.psu.edu>
2022-11-07 11:16:17 -0500

    f-stringify inheritance chain lookup plugin

--- plugins/lookup/inheritance_chain.py
+++ plugins/lookup/inheritance_chain.py
@@ -26,7 +26,7 @@ class LookupModule(LookupBase):
     def run(self, terms, variables=None, **kwargs):
         try:
             assert 'group_names' in variables, "Missing 'group_names' in variables"
-            assert len(terms) == 1, "Inheritance chain lookup plugin expects 1 term, got %s" % len(terms)
+            assert len(terms) == 1, f"Inheritance chain lookup plugin expects 1 term, got {len(terms)}"
         except AssertionError as exc:
             raise AnsibleError(str(exc))
         _vars = variables or {}
@@ -41,18 +41,14 @@ class LookupModule(LookupBase):
             _var = prefix + '_' + base
             _val = _vars.get(_var, [])
             if _val:
-                display.vvvv('%s = %s' % (_var, _val))
+                display.vvvv(f'{_var} = {_val}')
             try:
                 t = self._templar.template(
                     _vars.get(_var, []), preserve_trailing_newlines=True,
                     convert_data=True, escape_backslashes=False)
             except Exception as exc:
-                raise AnsibleError("Templating '%s' failed: %s" % (_var, str(exc)))
+                raise AnsibleError(f"Templating '{_var}' failed: {exc}")
             if t != _val:
-                display.vvvv('%s -> %s' % (_var, t))
+                display.vvvv(f'{_var} -> {t}')
             r.extend(t)
-        # this is actually not invalid if the name is valid, and there's no way
-        # to default in the playbook, so we'll just return an empty list here
-        #if not r:
-        #    raise AnsibleError("No inheritance chain variables exist for base '%s'" % base)
         return r
